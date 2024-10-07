6e471db830bdb16ff26f60dde9bbfa24fb240d09
brunobash <brunov.eng@gmail.com>
2024-10-07 19:11:07 -0300

    configuring some bullshit

---
+++ build/CMakeFiles/rest_api_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rest_api_example.dir/main.cpp.o"
+  "CMakeFiles/rest_api_example.dir/main.cpp.o.d"
+  "rest_api_example"
+  "rest_api_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rest_api_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/rest_api_example.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for rest_api_example.
+# This may be replaced when dependencies are built.
